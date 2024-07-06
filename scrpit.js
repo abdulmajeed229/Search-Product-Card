a5a5fa6304790125d2c7f8b2568b4eaee5b27c47
abdulmajeed229 <johnharris5934@gmail.com>
2024-07-06 10:55:37 +0500

    first commit

---
+++ scrpit.js
@@ -0,0 +1,306 @@
+let data = [{
+
+    Image: "https://freepngimg.com/thumb/car/3-2-car-free-download-png.png",
+    title: "Car",
+    description: "This is Example DescrptionThis is Example DescrptionThis is Example DescrptionThis is Example Descrption",
+    category: "Car",
+    price: "$10K"
+},
+
+{
+    Image: "https://www.tessllc.us/wp-content/uploads/2020/07/yacht-post-825x510.jpg",
+    title: "Boat",
+    description: "This is Example Descrption This is Example DescrptionThis is Example DescrptionThis is Example Descrption",
+    category: "Boat",
+    price: "$5K"
+},
+{
+    Image: "https://m.media-amazon.com/images/I/51uByLjsNEL._AC_UF894,1000_QL80_.jpg",
+    title: "Cycle",
+    description: "This is Example Descrption This is Example DescrptionThis is Example DescrptionThis is Example DescrptionThis is Example Descrption",
+    category: "Cycle",
+    price: "$500"
+},
+{
+    Image: "https://media.clondres.com/resize/uploads/sites/5/2018/08/londres-bus.jpg?format=auto",
+    title: "Bus",
+    description: "This is Example Descrption This is Example DescrptionThis is Example DescrptionThis is Example Descrption",
+    category: "Bus",
+    price: "$15K"
+}, {
+    Image: "https://propakistani.pk/wp-content/uploads/2023/12/iPhone-15-Pro-e1702381232998.jpg",
+    title: "Mobile",
+    description: "This is Example Descrption This is Example DescrptionThis is Example DescrptionThis is Example Descrption",
+    category: "Mobile",
+    price: "$1500"
+}, {
+    Image: "https://image.cnbcfm.com/api/v1/image/107405596-1713952166593-231228limxoutdoor.jpg?v=1715132212",
+    title: "Robot",
+    description: "This is Example Descrption This is Example DescrptionThis is Example DescrptionThis is Example Descrption",
+    category: "Robot",
+    price: "$20k"
+},
+{
+    Image: "https://mediarenditions.airbus.com/UQXu0AY25qoiBxydRuQqgV9LUp15ZPA3I-VZ1UAKAgU/resize?src=kpkp://airbus/38/601/601781-tmy3tjb0gw.jpg&w=480&h=480&t=fit",
+    title: "Helicopter",
+    description: "This is Example Descrption This is Example DescrptionThis is Example DescrptionThis is Example Descrption",
+    category: "Helicopter",
+    price: "$2M"
+},
+{
+    Image: "https://www.hondajet.com/-/media/HondaJet/Photos/News/Featured-News/News-Most-Delivered-5th-Year_375.jpg",
+    title: "Private Jet",
+    description: "This is Example Descrption This is Example DescrptionThis is Example DescrptionThis is Example Descrption",
+    category: "Jet",
+    price: "$3M"
+},
+{
+    Image: "https://e3.365dm.com/21/07/1600x900/skynews-boeing-737-plane_5435020.jpg?20210702173340",
+    title: "Airo Plane",
+    description: "This is Example Descrption This is Example Descrption This is Example Descrption This is Example Descrption ",
+    category: "Plane",
+    price: "$400M"
+},
+{
+    Image: "https://bd.gaadicdn.com/processedimages/suzuki/hayabusa/640X309/hayabusa6433f99fc006a.jpg",
+    title: "Heavy Bike",
+    description: "This is Example Descrption This is Example Descrption This is Example Descrption This is Example Descrption ",
+    category: "Bike",
+    price: "$12K"
+},
+{
+    Image: "https://assets.cat5.com/images/tactical-experts/rifle-basics-indentifying-parts-and-functions/bolt-action-rifle.jpg",
+    title: "Rifle",
+    description: "This is Example Descrption This is Example Descrption This is Example Descrption This is Example Descrption ",
+    category: "Rifle",
+    price: "$8K"
+},
+
+{
+
+    Image: "https://t4.ftcdn.net/jpg/04/72/07/09/360_F_472070989_b0jxzfspyQv1bFlWq8mJzPa2YWgWLWPn.jpg",
+    title: "Gun",
+    description: "This is Example Descrption This is Example Descrption This is Example Descrption This is Example Descrption ",
+    category: "Gun",
+    price: "$1K"
+} ,
+
+{
+
+    Image : "https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/Airbus_Beluga_Airbus_A300B4-608ST_F-GSTA_%2828858044414%29.jpg/1200px-Airbus_Beluga_Airbus_A300B4-608ST_F-GSTA_%2828858044414%29.jpg" ,
+
+    title : " Air Bus Beluga" ,
+
+    description :"This is Example Descrption This is Example Descrption This is Example Descrption This is Example Descrption " ,
+
+    category :"Plane" ,
+    print :"$300M"
+},
+{
+    Image : "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c4/Airbus_A400M_Atlas_%28ZM400%29_-_ASCOT482.jpg/1200px-Airbus_A400M_Atlas_%28ZM400%29_-_ASCOT482.jpg" ,
+
+    title : " Transport Aircraft" ,
+
+    description : "This is Example Descrption This is Example Descrption This is Example Descrption This is Example Descrption ",
+
+    category :"Plane" ,
+
+    price :"$500M"
+},
+
+{
+
+    Image : "https://i0.wp.com/internationalaviationhq.com/wp-content/uploads/2020/09/Smallest-private-jets.jpg" ,
+
+    title : " 10 Seater Jet" ,
+
+    description :"This is Example Descrption This is Example Descrption This is Example Descrption This is Example Descrption " ,
+
+    category :"Jet" ,
+    print :"$100M"
+},
+{
+
+    Image : "https://robbreport.com/wp-content/uploads/2019/07/adastra-1-courtesy-jochen-manz_orion-shuttleworth.jpg?w=1000" ,
+
+    title : "Future Luxury Yatch" ,
+
+    description :"This is Example Descrption This is Example Descrption This is Example Descrption This is Example Descrption " ,
+
+    category :"Boat" ,
+    print :"$600M"
+},
+{
+
+    Image : "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Bugatti_Chiron_1.jpg/800px-Bugatti_Chiron_1.jpg" ,
+
+    title : "Bugatti Chiron" ,
+
+    description :"This is Example Descrption This is Example Descrption This is Example Descrption This is Example Descrption " ,
+
+    category :"Car" ,
+    print :"$10M"
+},
+{
+
+    Image : "https://www.mansory.com/sites/default/files/styles/1920x800_fullwidth_car_slider/public/migrated/cars/Cars/rolls-royce/wraith/mansory_rolls_royce_wraith_25.jpg?itok=f5CcTOPO" ,
+
+    title : "Rolls Royce" ,
+
+    description :"This is Example Descrption This is Example Descrption This is Example Descrption This is Example Descrption " ,
+
+    category :"Car" ,
+    print :"$20M"
+},
+{
+
+    Image : "https://happycredit.in/cloudinary_opt/blog/opt-13phvg.webp" ,
+
+    title : " Racing Cycle  " ,
+
+    description :"This is Example Descrption This is Example Descrption This is Example Descrption This is Example Descrption " ,
+
+    category :"Cycle" ,
+    print :"$800"
+},
+{
+
+    Image : "https://live.staticflickr.com/8332/8144287214_e0d372343c_z.jpg" ,
+
+    title : " Luxury YouTung Bus" ,
+
+    description :"This is Example Descrption This is Example Descrption This is Example Descrption This is Example Descrption " ,
+
+    category :"Bus" ,
+    print :"$100K"
+},
+{
+
+    Image : "https://www.cnet.com/a/img/resize/128e1c28692dd03d094db9a2599477b8d0ad9d8d/hub/2022/02/07/1794135f-2ad0-40bd-9b9a-d49b2211b1ca/samsung-s22-preview-016.jpg?auto=webp&fit=crop&height=360&width=640" ,
+
+    title : " Samsung S22 Ultra" ,
+
+    description :"This is Example Descrption This is Example Descrption This is Example Descrption This is Example Descrption " ,
+
+    category :"Mobile" ,
+    print :"$2k"
+},
+
+{
+
+    Image : "https://japan-forward.com/wp-content/uploads/2020/10/Robot-in-Japan-021-e1603351031714.jpg" ,
+
+    title : "Japan Helping Robot" ,
+
+    description :"This is Example Descrption This is Example Descrption This is Example Descrption This is Example Descrption " ,
+
+    category :"Robot" ,
+    price :"Not For Sale"
+},
+
+{
+
+    Image : "https://upload.wikimedia.org/wikipedia/commons/9/9b/RNLAF_AH-64_Apache_at_the_Oirschotse_Heide_Low_Flying_Area_%2836570605232%29.jpg" ,
+
+    title : " US Helicopter" ,
+
+    description :"This is Example Descrption This is Example Descrption This is Example Descrption This is Example Descrption " ,
+
+    category :"Helicopter" ,
+    print :"$900M"
+},
+
+{
+
+    Image : "https://cdn.filestackcontent.com/AUcm0ze0SHM76x9FhaHgpz/resize=width:450,height:300/cache=expiry:max/https://www.filepicker.io/api/file/11dZJZnNTDy1qeHYZ80x" ,
+
+    title : "FNM M249" ,
+
+    description :"This is Example Descrption This is Example Descrption This is Example Descrption This is Example Descrption " ,
+
+    category :"Rifle" ,
+    print :"$100k"
+},
+];
+
+
+
+
+let main = document.getElementById('main');
+let opt = document.getElementById('choose');
+
+
+for (let i = 0; i < data.length; i++) {
+
+    main.innerHTML += `
+
+  <div id="card" >
+
+  <img src="${data[i].Image}">
+  <h1>${data[i].title}</h1>
+  <div>
+  <p>${data[i].description}</p>
+  </div>
+
+  <h3>${data[i].category}</h3>
+  <h4>${data[i].price}</h4>
+
+  <button id="btnnn">Add to Cart &nbsp; <i class="fa-solid fa-cart-shopping"></i> </button>
+
+</div>`
+}
+
+
+for( let b = 0 ; b < data.length ; b++){
+
+    if(opt.value ==="All"){
+
+        main.innerHTML += `
+
+  <div id="card" >
+
+  <img src="${data[i].Image}">
+  <h1>${data[i].title}</h1>
+  <div>
+  <p>${data[i].description}</p>
+  </div>
+
+  <h3>${data[i].category}</h3>
+  <h4>${data[i].price}</h4>
+
+  <button id="btnnn">Add to Cart &nbsp; <i class="fa-solid fa-cart-shopping"></i> </button>
+
+</div>`
+
+    }
+}
+
+opt.addEventListener('change', () => {
+
+    main.innerHTML = '';
+
+    for (let i = 0; i < data.length; i++) {
+
+        if (opt.value === data[i].category) {
+
+            main.innerHTML += `
+
+      <div id="card">
+      <img src="${data[i].Image}">
+      <h1>${data[i].title}</h1>
+      <div class="cardText">
+      <p>${data[i].description}</p>
+      </div>
+      <h3>${data[i].category}</h3>
+      <h4>Price: ${data[i].price}</h4>
+
+      <button id="btnnn" >Add to Cart &nbsp; <i class="fa-solid fa-cart-shopping"></i></button>
+    </div>`
+
+
+        }
+    }
+})
+
+
+
